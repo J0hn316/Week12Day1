201246cc3846e7421cdba2e423a610d1b8cbba22
J0hn316 <jcordell316@gmail.com>
2023-09-19 11:16:22 -0400

    Day2Lab

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const jsxEngine = require("jsx-view-engine");
+const port = 3000;
 
 const fruits = require("./models/fruits.js"); //NOTE: it must start with ./ if it's just a file, not an NPM package
 const vegetables = require("./models/vegetables.js");
@@ -10,6 +11,18 @@ const vegetables = require("./models/vegetables.js");
 app.set("view engine", "jsx");
 app.engine("jsx", jsxEngine());
 
+//near the top, around other app.use() calls
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log("I run for all routes");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("welcome to home page");
+});
+
 app.get("/fruits/", (req, res) => {
   // res.send(fruits);
   res.render("fruits/Index", { fruits: fruits });
@@ -17,6 +30,44 @@ app.get("/fruits/", (req, res) => {
 
 app.get("/vegetables/", (req, res) => {
   res.render("vegetables/Index", { vegetables: vegetables });
+});
+
+//New - get the form to add a new fruit.
+
+app.get("/fruits/new", function (req, res) {
+  res.render("fruits/New");
+});
+
+app.get("/vegetables/new", function (req, res) {
+  res.render("vegetables/New");
+});
+
+app.post("/fruits", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true; //do some data correction
+  } else {
+    //if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false; //do some data correction
+  }
+  fruits.push(req.body);
+  console.log(fruits);
+  // res.send("data received");
+  res.redirect("/fruits"); //send the user back to /fruits
+});
+
+app.post("/vegetables", (req, res) => {
+  if (req.body.readyToEat === "on") {
+    //if checked, req.body.readyToEat is set to 'on'
+    req.body.readyToEat = true; //do some data correction
+  } else {
+    //if not checked, req.body.readyToEat is undefined
+    req.body.readyToEat = false; //do some data correction
+  }
+  vegetables.push(req.body);
+  console.log(vegetables);
+  // res.send("data received");
+  res.redirect("/vegetables"); //send the user back to /vegetables
 });
 
 app.get("/fruits/:indexOfFruitsArray", (req, res) => {
@@ -33,6 +84,6 @@ app.get("/vegetables/:indexOfVegetablesArray", (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log("listening");
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
 });
